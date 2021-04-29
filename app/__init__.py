ae949237b476f16e7210ce54bb34fe1833f6c92a
Dimitri Sobolev <dmsobolev@yahoo.com>
2021-04-29 14:54:22 +0300

    refactor

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+
+
+def get_top_hashtags_and_links(country):
+    site = f"https://twitter-trends.iamrohit.in/{country}"
+    hdr = {'User-Agent': 'Mozilla/5.0'}
+    req = Request(site, headers=hdr)
+    page = urlopen(req)
+    soup = BeautifulSoup(page, "html.parser")
+
+    top_hashes_part = soup.find(id="copyData").find_all('a')
+
+    top_hastags = []
+    for item in top_hashes_part:
+        top_hastags.append(
+            {"hashtag": item.get_text(),
+             "link": item.get('href')}
+        )
+
+    return top_hastags
--- main.py
+++ main.py
@@ -1,25 +1,4 @@
-from urllib.request import Request, urlopen
-from bs4 import BeautifulSoup
-
-
-def get_top_hashtags_and_links(country):
-    site = f"https://twitter-trends.iamrohit.in/{country}"
-    hdr = {'User-Agent': 'Mozilla/5.0'}
-    req = Request(site, headers=hdr)
-    page = urlopen(req)
-    soup = BeautifulSoup(page, "html.parser")
-
-    top_hashes_part = soup.find(id="copyData").find_all('a')
-
-    top_hastags = []
-    for item in top_hashes_part:
-        top_hastags.append(
-            {"hashtag": item.get_text(),
-             "link": item.get('href')}
-        )
-
-    return top_hastags
-
+from app import get_top_hashtags_and_links
 
 if __name__ == '__main__':
     data = get_top_hashtags_and_links(country="indonesia")
